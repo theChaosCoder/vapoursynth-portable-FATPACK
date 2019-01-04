656071e0d79b280505d6e63150ea65f84caedfd6
Unknown <thechaoscoder@gmail.com>
2019-01-04 17:36:53 +0100

    Update script

--- create_7z.bat
+++ create_7z.bat
@@ -1,1 +1,2 @@
-VapourSynth64Portable\VapourSynth64\7z.exe a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=512m -mt=off -ms=off destination.7z Source
+VapourSynth64Portable\VapourSynth64\7z.exe a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=512m releases\VapourSynth64Portable_%date:~-4%_%date:~-7,2%_%date:~-10,2%.7z VapourSynth64Portable
+pause
